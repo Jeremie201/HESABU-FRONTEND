b3f0c5aac22af7a403294e173cff10b0cc783b74
Jeremie201 <132659288+Jeremie201@users.noreply.github.com>
2026-07-03 09:56:13 +0000

    Optimisation Open Graph et Twitter Cards

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -4,8 +4,8 @@ export default function SEO({
   title,
   description,
   keywords,
-  image = "/favicon.png",
-  url = "https://hesaburdc.com",
+  image = "https://hesabudrc.com/affiche.png",
+  url = "https://hesabudrc.com",
 }) {
   return (
     <Helmet>
@@ -27,13 +27,31 @@ export default function SEO({
         content="HESABU SARLU"
       />
 
+      <meta
+        name="robots"
+        content="index, follow"
+      />
+
+      <meta
+        name="language"
+        content="fr"
+      />
+
+      <meta
+        name="theme-color"
+        content="#dc2626"
+      />
+
       <link
         rel="canonical"
         href={url}
       />
 
       {/* Open Graph */}
-      <meta property="og:type" content="website" />
+      <meta
+        property="og:type"
+        content="website"
+      />
 
       <meta
         property="og:title"
@@ -51,6 +69,11 @@ export default function SEO({
       />
 
       <meta
+        property="og:image:alt"
+        content="HESABU SARLU - Solutions GPS et Gestion de flotte"
+      />
+
+      <meta
         property="og:url"
         content={url}
       />
@@ -58,6 +81,11 @@ export default function SEO({
       <meta
         property="og:site_name"
         content="HESABU SARLU"
+      />
+
+      <meta
+        property="og:locale"
+        content="fr_CD"
       />
 
       {/* Twitter */}
@@ -79,6 +107,11 @@ export default function SEO({
       <meta
         name="twitter:image"
         content={image}
+      />
+
+      <meta
+        name="twitter:image:alt"
+        content="HESABU SARLU"
       />
     </Helmet>
   );
