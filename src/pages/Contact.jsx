b29b440fb6275cb709e2fb2ecab20b5850c458ff
Jeremie201 <132659288+Jeremie201@users.noreply.github.com>
2026-07-03 07:42:26 +0000

    Ajout du SEO sur la page Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,16 @@
 import Navbar from "../components/Navbar";
 import ContactSection from "../components/ContactSection";
 import Footer from "../components/Footer";
+import SEO from "../components/SEO";
 
 function Contact() {
   return (
     <>
+      <SEO
+        title="Contact | HESABU SARLU"
+        description="Contactez HESABU SARLU pour vos besoins en solutions GPS, gestion de flotte, télématique embarquée et IoT en RDC."
+        keywords="contact HESABU, GPS RDC, gestion de flotte, support client, IoT RDC"
+      />
       <Navbar />
       <ContactSection />
       <Footer />
