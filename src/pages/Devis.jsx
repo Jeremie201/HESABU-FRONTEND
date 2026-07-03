01b49317f37a06fa51458ff7b2f3789258d213cb
Jeremie201 <132659288+Jeremie201@users.noreply.github.com>
2026-07-03 07:49:21 +0000

    Ajout du SEO sur la page Devis

--- src/pages/Devis.jsx
+++ src/pages/Devis.jsx
@@ -1,10 +1,16 @@
 import Navbar from "../components/Navbar";
 import DevisSection from "../components/DevisSection";
 import Footer from "../components/Footer";
+import SEO from "../components/SEO";
 
 function Devis() {
   return (
     <>
+      <SEO
+        title="Contact | HESABU SARLU"
+        description="Contactez HESABU SARLU pour vos besoins en solutions GPS, gestion de flotte, télématique embarquée et IoT en RDC."
+        keywords="contact HESABU, GPS RDC, gestion de flotte, support client, IoT RDC"
+      />
       <Navbar />
       <DevisSection />
       <Footer />
