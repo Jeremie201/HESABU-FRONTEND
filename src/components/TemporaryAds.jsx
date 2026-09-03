f10c64122bd59695e3b1c518cdfce8d11089f4a1
Jeremie201 <132659288+Jeremie201@users.noreply.github.com>
2026-09-03 15:52:37 +0100

    Fix typo in ad label for school year 2026-2027

--- src/components/TemporaryAds.jsx
+++ src/components/TemporaryAds.jsx
@@ -16,7 +16,7 @@ const ads = [
   {
     id: 6,
     image: rentreeImage,
-    label: "SPÉCIAL RENTRÉE SCOLAIRE 2026-2027",
+    label: "SPÉCIALE RENTRÉE SCOLAIRE 2026-2027",
     title: "La sécurité de vos déplacements et ceux de vos enfants, notre priorité.",
     description:
       "Ne vous contentez plus d’attendre leur arrivée ou leur arrivée.  Avec HESABU, suivez en temps réel les déplacements de vos enfants et de vos véhicules, où que vous soyez.",
