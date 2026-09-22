907e783832aa2c71ac7df1c3e295c7454c18db8c
Jeremie201 <132659288+Jeremie201@users.noreply.github.com>
2026-09-22 16:32:02 +0100

    Remove StructuredData component from main.jsx
    Removed StructuredData component from rendering.

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { Toaster } from "react-hot-toast";
+
 import App from "./App";
 import "./index.css";
-import StructuredData from "./components/StructuredData";
 
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
   <React.StrictMode>
     <HelmetProvider>
-
-      <StructuredData />
 
       <Toaster
         position="top-right"
