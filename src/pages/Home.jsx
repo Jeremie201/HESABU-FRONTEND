d1580e218f5af9cbf09a1d5e20624b0a58e909b5
Jeremie201 <132659288+Jeremie201@users.noreply.github.com>
2026-09-22 16:47:49 +0100

    Modify SEO keywords in Home.jsx
    Updated SEO keywords for improved search relevance.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
       <SEO
         title="HESABU SARLU | Solutions GPS et Gestion Intelligente de Flotte en RDC"
         description="HESABU SARLU est une entreprise congolaise spécialisée dans les solutions GPS, la gestion intelligente de flotte, la télématique embarquée et les technologies IoT."
-        keywords="GPS RDC, GPS Kinshasa gestion de flotte, télématique embarquée, IoT, géolocalisation, localisation véhicules, gestion à distance, suivi en temps réel, suivi de véhicules, HESABU, HESABU SARLU"
+        keywords="GPS RDC, GPS Kinshasa, gestion de flotte, télématique embarquée, IoT, géolocalisation, localisation véhicules, gestion à distance, suivi en temps réel, suivi de véhicules, HESABU, HESABU SARLU, IoT, Tracking véhicule, Télématique, HESABU, Kinshasa, République Démocratique du Congo"
         image="https://hesabudrc.com/hesabusend.png"
         url="https://hesabudrc.com/"
       />
