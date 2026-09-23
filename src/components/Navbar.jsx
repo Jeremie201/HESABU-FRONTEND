ac14a74b529d425b9e4fce578090c69fa37889c6
Jeremie201 <132659288+Jeremie201@users.noreply.github.com>
2026-09-23 14:31:35 +0100

    Change logo format from PNG to WEBP

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import logo from "../assets/hero.png";
+import logo from "../assets/hero.webp";
 
 function Navbar() {
 
